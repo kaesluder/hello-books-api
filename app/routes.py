041a200706cef82ae53e0e0cbc4bb15902fd3eac
Kae Sluder <kaesluder@fastmail.com>
2022-10-28 11:44:03 -0400

    New books route using database

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
+from app import db
 from app.models.book import Book
-from flask import Blueprint, jsonify, abort, make_response
+from flask import Blueprint, jsonify, abort, make_response, request
 
 
-books_bp = Blueprint("books_bp", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+@books_bp.route("", methods=["GET", "POST"])
+def handle_books():
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return make_response(f"Book {new_book.title} successfully created", 201)
