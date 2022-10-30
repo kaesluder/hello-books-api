068565c6059f373a2ddb8c0a6e1c4bcf06b1f16c
Kae Sluder <kaesluder@fastmail.com>
2022-10-30 11:29:52 -0400

    validate book and update book

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from app import db
 from app.models.book import Book
 from flask import Blueprint, jsonify, abort, make_response, request
@@ -25,3 +26,36 @@ def handle_books_post():
     db.session.commit()
 
     return make_response(f"Book {new_book.title} successfully created", 201)
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    book = Book.query.get(book_id)
+
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+
+    return book
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_single_book(book_id):
+    book = validate_book(book_id)
+    return book.to_dict()
+
+
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
+
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully updated")
+
